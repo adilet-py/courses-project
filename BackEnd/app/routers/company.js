ce94557a2235dff94ff3d442b5fd297fe30ff068
songhee24 <44497623+songhee24@users.noreply.github.com>
2019-06-04 19:20:52 +0600

    some

--- BackEnd/app/routers/company.js
+++ BackEnd/app/routers/company.js
@@ -1,22 +1,18 @@
 module.exports = (app) => {
     let companyController = require('../controllers/companyController');
     let auth = require('../middleware/auth');
-
     /**
      * User registration
      */
     app.post('/company/register', companyController.register);
-
     /**
      * User authorization
      */
     app.post('/company/login', companyController.login);
-
     /**
      * User forgot password
      */
     app.post('/company/forgot-password', companyController.forgot_password);
-
     /**
      * Get me
      */
